e33270288e2f26268a93a31a79195cf66279df58
Awa Gadiaga <gadiagaawa22@gmail.com>
2025-08-20 16:42:00 +0000

    Mise à jour views.html pour Render

--- gestion/views.py
+++ gestion/views.py
@@ -1246,7 +1246,7 @@ def liste_bons_commande(request):
 
 ####################################################################################################
 def accueil(request):
-    return render(request, 'gestion/login.html')
+    return render(request, 'gestion/home.html')
 
 def ajouter_compte(request):
     if request.method == 'POST':
